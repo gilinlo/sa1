c848d387934820dbef37fdd2d28ce0c54f56341b
winnieywhsiao <49788032+winnieywhsiao@users.noreply.github.com>
2019-05-09 01:35:14 +0800

    w commit 2

--- app/src/main/java/com/example/login/LoginActivity.java
+++ app/src/main/java/com/example/login/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.login;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -82,11 +83,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_login_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+        Button mEmailLogInButton = findViewById(R.id.email_login_button);
+
+        mEmailLogInButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                attemptLogin();
+            public void onClick(View v) {
+                Intent intent = new Intent(); intent.setClass(LoginActivity.this, IndexActivity.class);
+                startActivity(intent);
             }
         });
 
