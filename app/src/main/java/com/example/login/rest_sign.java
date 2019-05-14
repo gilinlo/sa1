e8e510d2bf515e1b307737dad57711d077905bb9
melissalee0609 <49824396+melissalee0609@users.noreply.github.com>
2019-05-14 15:17:38 +0800

    restsign

---
+++ app/src/main/java/com/example/login/rest_sign.java
@@ -0,0 +1,15 @@
+package com.example.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class rest_sign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rest_sign);
+
+
+    }
+}
